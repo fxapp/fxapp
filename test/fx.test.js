6ca0d76fae012fcb0cea7bd67b1ea820988a9fbf
Wolfgang <wolf@okwolf.com>
2018-03-03 19:42:44 -0800

    empty fx already handled just needed a test

--- test/fx.test.js
+++ test/fx.test.js
@@ -2,6 +2,15 @@ import { fxapp } from "../src";
 
 const invalidFx = ["invalid", { ignore: "me" }];
 
+test("ignore empty fx", () => {
+  const actions = fxapp({
+    actions: {
+      invalid: () => []
+    }
+  });
+  expect(actions.invalid()).toEqual([]);
+});
+
 test("ignore invalid fx", () => {
   const actions = fxapp({
     actions: {
