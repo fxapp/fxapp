f71d43e67c08fd2ff1cf004f6084901cbfa3ccd1
Wolfgang <wolf@okwolf.com>
2018-03-05 20:45:25 -0800

    turns out i was way overthinking the h function now works with jsx

--- src/h.js
+++ src/h.js
@@ -4,25 +4,9 @@ export function h() {
   if (isFn(arguments[0])) {
     return arguments[0](arguments[1] || {}, arguments[2]);
   }
-  var vnode = [arguments[0]];
-  var index = 1;
-  if (
-    arguments[index] === Object(arguments[index]) &&
-    !Array.isArray(arguments[index])
-  ) {
-    vnode.push(arguments[index]);
-    index++;
-  }
-  var children = [];
-  for (; index < arguments.length; index++) {
-    var child = arguments[index];
-    if (child !== undefined) {
-      children.push(child);
-    }
-  }
-  if (children.length) {
-    vnode.push(children);
-  }
+  var vnode = [].filter.call(arguments, function(node) {
+    return node !== undefined;
+  });
 
   return vnode;
 }
--- test/h.test.js
+++ test/h.test.js
@@ -2,21 +2,27 @@ import { h } from "../src";
 
 test("empty vnode", () => expect(h("div")).toEqual(["div"]));
 
-test("vnode with a single child", () => {
-  expect(h("div", "foo")).toEqual(["div", ["foo"]]);
-});
+test("vnode with a single child", () =>
+  expect(h("div", "foo")).toEqual(["div", "foo"]));
 
 test("positional String/Number children", () => {
-  expect(h("div", "foo", "bar", "baz")).toEqual(["div", ["foo", "bar", "baz"]]);
+  expect(h("div", "foo", "bar", "baz")).toEqual(["div", "foo", "bar", "baz"]);
 
   expect(h("div", 0, "foo", 1, "baz", 2)).toEqual([
     "div",
-    [0, "foo", 1, "baz", 2]
+    0,
+    "foo",
+    1,
+    "baz",
+    2
   ]);
 
   expect(h("div", "foo", h("div", "bar"), "baz", "quux")).toEqual([
     "div",
-    ["foo", ["div", ["bar"]], "baz", "quux"]
+    "foo",
+    ["div", "bar"],
+    "baz",
+    "quux"
   ]);
 });
 
@@ -29,41 +35,83 @@ test("vnode with props", () => {
     }
   };
 
-  expect(h("div", props, "baz")).toEqual(["div", props, ["baz"]]);
+  expect(h("div", props, "baz")).toEqual(["div", props, "baz"]);
 });
 
 test("pass through Boolean children", () => {
-  expect(h("div", true)).toEqual(["div", [true]]);
-  expect(h("div", false)).toEqual(["div", [false]]);
+  expect(h("div", true)).toEqual(["div", true]);
+  expect(h("div", false)).toEqual(["div", false]);
 });
 
-test("pass through null children", () => {
-  expect(h("div", null)).toEqual(["div", [null]]);
-});
+test("pass through null children", () =>
+  expect(h("div", null)).toEqual(["div", null]));
 
-test("skip undefined children", () => {
-  expect(h("div", undefined)).toEqual(["div"]);
-});
+test("skip undefined children", () =>
+  expect(h("div", undefined)).toEqual(["div"]));
 
 test("component with no props", () => {
   const Component = ({ name = "world" }) => h("div", "Hello " + name);
-  expect(h(Component)).toEqual(["div", ["Hello world"]]);
+  expect(h(Component)).toEqual(["div", "Hello world"]);
 });
 
 const Component = h.bind(null, "div");
 
-test("component with props", () => {
+test("component with props", () =>
   expect(h(Component, { id: "foo" }, "bar")).toEqual([
     "div",
     { id: "foo" },
-    ["bar"]
-  ]);
-});
+    "bar"
+  ]));
 
 test("nested components with props", () => {
   expect(h(Component, { id: "foo" }, h(Component, { id: "bar" }))).toEqual([
     "div",
     { id: "foo" },
-    [["div", { id: "bar" }]]
+    ["div", { id: "bar" }]
   ]);
 });
+
+test("JSX output", () =>
+  expect(
+    h(
+      "main",
+      null,
+      h("h1", null, 1),
+      h(
+        "button",
+        {
+          onclick: [
+            "action",
+            {
+              path: "down"
+            }
+          ]
+        },
+        "-"
+      ),
+      h(
+        "button",
+        {
+          onclick: [
+            "action",
+            {
+              path: "up"
+            }
+          ]
+        },
+        "+"
+      )
+    )
+  ).toEqual([
+    "main",
+    null,
+    ["h1", null, 1],
+    ["button", { onclick: ["action", { path: "down" }] }, "-"],
+    [
+      "button",
+      {
+        onclick: ["action", { path: "up" }]
+      },
+      "+"
+    ]
+  ]));
