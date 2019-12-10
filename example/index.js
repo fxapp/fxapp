fbb9d1ed860e5b1fc4abd3a37c5aee9f392a8a83
Wolfgang <wolf@okwolf.com>
2019-12-09 21:45:35 -0800

    add crud routes to example

--- example/index.js
+++ example/index.js
@@ -24,8 +24,79 @@ const fallBackResponse = () => ({
   }
 });
 
+const GetHeros = ({ heros }) => ({ response: { json: heros } });
+const GetHero = ({
+  request: {
+    params: { id }
+  },
+  heros
+}) => {
+  const hero = heros.find(hero => hero.id == id);
+  return hero
+    ? { response: { json: hero } }
+    : { response: { statusCode: 404 } };
+};
+const AddHero = ({ request: { jsonBody }, heros }) => {
+  if (!jsonBody || !jsonBody.name) {
+    return { response: { statusCode: 400 } };
+  }
+  const hero = {
+    id: heros.length,
+    ...jsonBody
+  };
+  return {
+    response: { statusCode: 201, json: hero },
+    heros: heros.concat(hero)
+  };
+};
+const UpdateHero = ({
+  request: {
+    params: { id },
+    jsonBody
+  },
+  heros
+}) => {
+  if (!jsonBody) {
+    return { response: { statusCode: 400 } };
+  }
+  let heroUpdated = false;
+  const updatedHeros = heros.map(hero => {
+    if (hero.id == id) {
+      heroUpdated = true;
+      return {
+        ...hero,
+        ...jsonBody
+      };
+    }
+    return hero;
+  });
+  return heroUpdated
+    ? { heros: updatedHeros, response: { statusCode: 204 } }
+    : { response: { statusCode: 404 } };
+};
+const RemoveHero = ({
+  request: {
+    params: { id }
+  },
+  heros
+}) => {
+  const updatedHeros = heros.filter(hero => hero.id != id);
+  return updatedHeros.length !== heros.length
+    ? { heros: updatedHeros, response: { statusCode: 204 } }
+    : { response: { statusCode: 404 } };
+};
+
 const routes = {
   _: fallBackResponse,
+  heros: {
+    GET: GetHeros,
+    POST: AddHero,
+    $id: {
+      GET: GetHero,
+      PUT: UpdateHero,
+      DELETE: RemoveHero
+    }
+  },
   test: {
     special: {
       path: () => ({
@@ -36,15 +107,28 @@ const routes = {
       })
     },
     $id: {
-      get: addJsonResponse,
-      post: delay({ ms: 2000, action: addJsonResponse })
+      GET: addJsonResponse,
+      POST: delay({ ms: 2000, action: addJsonResponse })
     }
   }
 };
 
 app({
+  state: {
+    count: 0,
+    heros: [
+      {
+        id: 0,
+        name: "Superman"
+      },
+      {
+        id: 1,
+        name: "Flash"
+      }
+    ]
+  },
   routes,
-  customFx: ({ count = 0 }) => ({
+  customFx: ({ count }) => ({
     other: "state",
     count: count + 1
   })
